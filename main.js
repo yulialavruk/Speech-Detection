b0f9fb0c250ad905ce950ee11e08c2cf358a7954
yulialavruk <41869512+yulialavruk@users.noreply.github.com>
2018-07-31 13:05:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;	// Chrome supports speech recognition with prefixed properties. Firefox and other non-prefix browsers
 
-  const recognition = new SpeechRecognition();
-  recognition.interimResults = true;
-  recognition.lang = 'en-US';
+  const recognition = new SpeechRecognition();	// create Speech Recognition
+  recognition.interimResults = true;	// whether interim results should be returned
+  recognition.lang = 'en-US';	// set the language of the current SpeechRecognition
   
-  let p = document.createElement('p');
-  const words = document.querySelector('.words');
-  words.appendChild(p);
+  let p = document.createElement('p');	// in an HTML document, create an element named tagName
+  const words = document.querySelector('.words');	// returns the first Element within the document that matches the specified selector
+  words.appendChild(p);	// adds a node to the end of the list of children of a specified parent node. 
 
-  recognition.addEventListener('result', e => {
+  recognition.addEventListener('result', e => {	// use  callback to process the results
     const transcript = Array.from(e.results)
       .map(result => result[0])
       .map(result => result.transcript)
@@ -24,4 +24,4 @@
 
   recognition.addEventListener('end', recognition.start);
 
-  recognition.start();
+  recognition.start(); // launches speech recognition
